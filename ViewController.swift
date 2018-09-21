ab8821441cf17185dd82f36f49e9bc36d389f7f8
安井春輝 <yasui.haruki.74s@gmail.com>
2018-09-21 21:48:17 +0700

    add mainView

--- ViewController.swift
+++ ViewController.swift
@@ -13,7 +13,22 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.view.addSubview(mainView)
+        mainView.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 100).isActive = true
+        mainView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 20).isActive = true
+        mainView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: -100).isActive = true
+        mainView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -20).isActive = true
     }
+    
+    let mainView: UIView = {
+        let view = UIView()
+        //autolayoutが自動で入るのをfalseにして、コードでautolayoutができるようにする
+        view.translatesAutoresizingMaskIntoConstraints = false
+        //orange色をbackgroudColorとして設定
+        view.backgroundColor = UIColor(red:254/255, green:227/255, blue:160/255, alpha:1)
+        view.layer.cornerRadius = 30
+        return view
+    }()
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
