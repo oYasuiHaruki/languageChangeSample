1bbf9965c3e7743bc839c5cd93a099767ffe0fd1
安井春輝 <yasui.haruki.74s@gmail.com>
2018-09-21 21:04:41 +0700

    first commit

---
+++ Language.swift
@@ -0,0 +1,26 @@
+//
+//  Language.swift
+//  languageChangeSample
+//
+//  Created by 安井春輝 on 9/21/30 H.
+//  Copyright © 30 Heisei haruki yasui. All rights reserved.
+//
+
+import Foundation
+
+struct English {
+    
+    static let dictionary =
+        ["Hello": "Hello",
+         "Good morning": "Good morning"]
+}
+
+struct Japanese {
+    static let dictionary =
+        ["Hello": "こんにちは",
+         "Good morning": "おはようございます"]
+}
+
+class Language {
+//    static let userDefault
+}
